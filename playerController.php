04c032210b70fa6e0dce39e1412c63288376bf73
Christian Howard <christian.john.howard@gmail.com>
2016-11-07 13:36:23 -0500

    Update playerController.php

--- playerController.php
+++ playerController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\lara_player;
 
 class playerController extends Controller
 {
