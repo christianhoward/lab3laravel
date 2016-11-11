fba5de840116b74afc07e3f2053e02b7b119bf95
Christian Howard <christian.john.howard@gmail.com>
2016-11-11 14:11:34 -0500

    Update lara_player.php

--- lara_player.php
+++ lara_player.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class lara_player extends Model
 {
     protected $table = 'lara_players';
+    protected $fillable = [
+        'playername',
+        'age',
+        'city',
+        'country',
+        'gender',
+        'handedness',
+        'broom',
+        'position',
+        'team',
+        'favoritecolor',
+        'headshot'
+    ];
 }
